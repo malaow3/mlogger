fefb7ac00fedd8d62d2c71e69c1f33638a128064
malaow3 <malaow3@yahoo.com>
2022-02-08 22:42:54 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlogger',
+    version='0.1.0',
+    author='malaow3',
+    author_email='malaow3@yahoo.com',
+    packages=['mlogger'],
+    description='A loguru setup module',
+    install_requires=[
+        "loguru",
+    ],
+)
