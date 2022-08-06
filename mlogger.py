089ab1bea4937cef287681d5b6010b6d7248ff80
malaow3 <malaow3@yahoo.com>
2022-08-05 22:25:51 -0400

    update logging to include extra fields

--- mlogger.py
+++ mlogger.py
@@ -4,9 +4,12 @@ import sys
 
 def init_logger():
     logger.remove()
+    fmt = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {function} - {file}:{line} - {message}"
+    if len(logger.__dict__["_options"][-1]) != 0:
+        fmt = fmt + " - {extra}"
     logger.add(
         sys.stdout,
-        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {function} - {file}:{line} - {message} | {extra}",  # noqa
+        format=fmt,  # noqa
         level="INFO",
     )
     return logger
