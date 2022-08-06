6024d4a5c6e760c1131bd4376f5d5d6a89c97a1a
malaow3 <malaow3@yahoo.com>
2022-08-06 09:48:07 -0400

    Use pylogrus

--- mlogger.py
+++ mlogger.py
@@ -2,6 +2,33 @@ from loguru import logger
 import sys
 
 
+import logging
+from pylogrus import PyLogrus, TextFormatter
+
+
+def get_logger():
+    logging.setLoggerClass(PyLogrus)
+
+    logger = logging.getLogger(__name__)  # type: PyLogrus
+    logger.setLevel(logging.DEBUG)
+
+    # get file line and function
+    format = (
+        "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(funcName)s - %(message)s"
+    )
+    formatter = TextFormatter(fmt=format, datefmt="Z", colorize=True)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
+log: PyLogrus = get_logger()
+
+
 def init_logger(use_extra: bool = False):
     logger.remove()
     fmt = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {function} - {file}:{line} - {message}"
